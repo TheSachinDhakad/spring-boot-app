34a543d7dda6b4260998b1287d4abd47fd08be11
thesachindhakad <sachindhakad7265@gmail.com>
2024-07-09 02:40:44 +0530

    jwt implement

--- src/main/java/com/josh/config/AppConfig.java
+++ src/main/java/com/josh/config/AppConfig.java
@@ -1,39 +1,4 @@
-//package com.josh.config;
-//
-//import javax.security.sasl.AuthorizeCallback;
-//
-//import org.springframework.boot.autoconfigure.integration.IntegrationProperties.Management;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class AppConfig {
-//	
-//	@Bean
-//	SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
-//		
-//		httpSecurity.sessionManagement(management->management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//		.authorizeHttpRequests(Authorize -> Authorize
-//				.requestMatchers("/api/**").authenticated().anyRequest().permitAll())
-//		.addFilterBefore(new JwtValidtor(), BasicAuthenticationFilter.class)
-//		.csrf(csrf -> csrf.disable());
-//	
-//		return httpSecurity.build();
-//		
-//	}
-//@Bean
-//PasswordEncoder passwordEncoder() {
-//	return new BCryptPasswordEncoder();
-//}
-//}
+
 package com.josh.config;
 
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/josh/config/JwtValidtor.java
+++ src/main/java/com/josh/config/JwtValidtor.java
@@ -18,39 +18,6 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-//
-//public class JwtValidtor extends OncePerRequestFilter {
-//	
-//
-//	@Override
-//	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//			throws ServletException, IOException {
-//		String jwt = request.getHeader(JwtContant.JWT_HEADER);
-//		
-//		if(jwt != null) {
-//			
-//			try {
-//				String email = JwtProvider.getEmailFormJwtToken(jwt);
-//				List<GrantedAuthority> authorities = new ArrayList<>(); 
-//				
-//				Authentication authentication = new UsernamePasswordAuthenticationToken(email,null ,  authorities);
-//				SecurityContextHolder.getContext().setAuthentication(authentication);
-//				
-//				
-//			} catch (Exception e) {
-//				throw new BadCredentialsException("invalid token");
-//			}
-//		}
-//		
-//		
-//		
-//		filterChain.doFilter(request, response);
-//		
-//	}
-//
-//	
-//
-//}
 
 
 
