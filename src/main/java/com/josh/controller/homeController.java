ae8a419b55bea6c1ed9d808e780d50e8aa0e1a61
sachin dhakad <103819602+TheSachinDhakad@users.noreply.github.com>
2024-07-10 21:42:56 +0530

    Update homeController.java

--- src/main/java/com/josh/controller/homeController.java
+++ src/main/java/com/josh/controller/homeController.java
@@ -8,6 +8,6 @@ public class homeController {
 
     @GetMapping("/")
     public String homeController() {
-        return "Welcome, create your own social media app using my API. Endpoint: https://spring-boot-app-production-f847.up.railway.app/";
+        return "Welcome, create your own social media app using my API. Endpoint: https://github.com/TheSachinDhakad/spring-boot-app/blob/main/README.md";
     }
 }
